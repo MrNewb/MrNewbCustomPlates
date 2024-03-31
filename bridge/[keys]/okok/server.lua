2018ca18c98c5cc2df71c04b2c5973071480b3e6
Donivan <donivansee@live.com>
2024-03-31 15:45:51 -0400

    maybe this will help with the door being locked?
    Lucked out already passing entityid

--- bridge/[keys]/okok/server.lua
+++ bridge/[keys]/okok/server.lua
@@ -3,11 +3,13 @@ if Config.Debug then print("Keys Set To ", Config.Keys) end
 
 -- submitted by Arkosee / https://github.com/Arkosee
 function RemoveOldKeys(src, entityid, oldplate, newplate)
+	SetVehicleDoorsLocked(entityid, 0)
 	TriggerClientEvent('okokGarage:removeKeys', src, oldplate)
 	--print('Removed keys for '.. oldplate)
 end
 
 function GiveKeys(src, entityid, oldplate, newplate)
+	SetVehicleDoorsLocked(entityid, 0)
 	TriggerClientEvent("okokGarage:GiveKeys", newplate, src)
 	--print('Gave keys for '.. newplate)
 end
