33c5bf963c828dc20cc8c4a832e460d0e73f5dd3
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-01-17 23:53:27 -0500

    Update server.lua

--- bridge/[inventory]/qb-inventory/server/server.lua
+++ bridge/[inventory]/qb-inventory/server/server.lua
@@ -4,9 +4,9 @@ local version = Config.OverrideQBInventoryV2
 
 function UpdateVehicleInventoryTrunkGlove(src, oldplate, newplate)
 	if version then
-		MySQL.update.await('UPDATE inventories SET identifier = CONCAT("glovebox-", ?), items = items WHERE identifier = CONCAT("glovebox-", ?)', { newplate, oldplate })
-		MySQL.update.await('UPDATE inventories SET identifier = CONCAT("trunk-", ?), items = items WHERE identifier = CONCAT("trunk-", ?)', { newplate, oldplate })
-		if Config.Debug then print("Updated Plate from "..oldplate.." to "..newplate) end
+		--MySQL.update.await('UPDATE inventories SET identifier = CONCAT("glovebox-", ?), items = items WHERE identifier = CONCAT("glovebox-", ?)', { newplate, oldplate })
+		--MySQL.update.await('UPDATE inventories SET identifier = CONCAT("trunk-", ?), items = items WHERE identifier = CONCAT("trunk-", ?)', { newplate, oldplate })
+		--if Config.Debug then print("Updated Plate from "..oldplate.." to "..newplate) end
 		-- I dont want to study this inventory to understand how to update the items. This should prevent the qb-inventory v2
 	else
 		MySQL.update.await('UPDATE inventory_glovebox SET plate = ? WHERE plate = ?', { newplate, oldplate })
